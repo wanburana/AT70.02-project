b2bb3b5770d24b854284efc05d867d6c38825cfa
Jirasak Buranathawornsom <wanjirasak@gmail.com>
2021-11-07 14:42:12 +0700

    fix some error, add a-star algorithm

--- project.py
+++ project.py
@@ -24,7 +24,7 @@ class Spot:
         self.x = row * width
         self.y = col * width
         self.color = WHITE
-        self.neightbors = []
+        self.neighbors = []
         self.width = width
         self.total_rows = total_rows
 
@@ -70,16 +70,19 @@ class Spot:
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
-    def update_neightbors(self, grid):
-        self.neightbors = []
+    def update_neighbors(self, grid):
+        self.neighbors = []
         if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # DOWN
-            self.neightbors.append(grid[self.row + 1][self.col])
-        elif self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
-            self.neightbors.append(grid[self.row - 1][self.col])
-        elif self.col < self.total_row - 1 and not grid[self.row][self.col + 1].is_barrier(): # RIGHT
-            self.neightbors.append(grid[self.row][self.col + 1])
-        elif self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
-            self.neightbors.append(grid[self.row][self.col - 1])
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # RIGHT
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
+            self.neighbors.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
         return False
@@ -89,7 +92,7 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
 
-    return abs(x1 - y1) + abs(x2 - y2)
+    return abs(x1 - x2) + abs(y1 - y2)
 
 def make_grid(rows, width):
     grid = []
@@ -131,11 +134,11 @@ def algorithm(draw, grid, start, end):
     count = 0
     open_set = PriorityQueue()
     open_set.put((0, count, start))
-    came_from = {}
-    g_score = {spot:float('inf') for row in grid for spot in row}
+    came_from = {} # keep track of the previous spot 
+    g_score = {spot: float('inf') for row in grid for spot in row} # initially assign every spot distance as infinity
     g_score[start] = 0
-    f_score = {spot:float('inf') for row in grid for spot in row}
-    f_score[start] = h(start.get_pos(), end.get_pos())
+    f_score = {spot: float('inf') for row in grid for spot in row} 
+    f_score[start] = h(start.get_pos(), end.get_pos())    
 
     open_set_hash = {start}
 
@@ -145,11 +148,29 @@ def algorithm(draw, grid, start, end):
                 pygame.quit()
         current = open_set.get()[2]
         open_set_hash.remove(current)
+        if current == end:
+            return True
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
 
-        
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
+
 def main(win, width):
     ROWS = 50
-    grid = make_grid(ROWS, width)
+    grid = make_grid(ROWS, width) # create (invisible) grid template
 
     start = None
     end = None
@@ -158,9 +179,9 @@ def main(win, width):
     started = False
 
     while run:
-        draw(WIN, grid, ROWS, WIDTH)
+        draw(WIN, grid, ROWS, WIDTH) # draw grid and other coloring
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: 
                 run = False
             if started:
                 continue
@@ -191,9 +212,10 @@ def main(win, width):
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and not started:
+                    # for every spot in grid, put its valid adjecent spot to its neighbors list
                     for row in grid:
                         for spot in row:
-                            spot.update_neighbors()
+                            spot.update_neighbors(grid) 
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
     pygame.quit()
